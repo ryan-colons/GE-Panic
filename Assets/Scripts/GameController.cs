c126824c11928e6bcbb2455270db8b14853e27b2
rycoll <colry349@gmail.com>
2017-08-19 11:24:46 +1200

    Added a GameController object

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	private void Awake () {
+		//allows this object to persist between scenes
+		DontDestroyOnLoad (this.gameObject);
+	}
+
+	private void startGame () {
+		SceneManager.LoadScene ("arena");
+	}
+
+	private void exitApplication () {
+		Application.Quit ();
+	}
+}
