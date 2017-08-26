d2fa2469eb8a98782cfb1d24b9ef780065a85df3
rycoll <colry349@gmail.com>
2017-08-26 13:43:40 +1200

    Reworked respawning, added boosting

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -7,28 +7,37 @@ public class PlayerControl : MonoBehaviour {
 	public float speed;
 	private Rigidbody body;
 	private Vector3 startPos;
-	public string horizInput, vertInput;
+	public string horizInput, vertInput, boostInput;
+	private float boostTimeout;
+	public float maxBoostTimeout = 10f;
 
-	void Start () {
+	private void Start () {
 		body = GetComponent<Rigidbody> ();
 		startPos = transform.position;
 	}
 
-	void FixedUpdate () {
+	private void FixedUpdate () {
 		//apparently FixedUpdate() should be used with rigidbodies, instead of Update()
 		float horiz_move = Input.GetAxis(horizInput);
 		float vert_move = Input.GetAxis (vertInput);
 
+		if (Input.GetButton (boostInput) && boostTimeout <= 0f) {
+			horiz_move *= 50f;
+			vert_move *= 50f;
+			boostTimeout = maxBoostTimeout;
+		}
+
 		Vector3 move = new Vector3 (horiz_move, 0f, vert_move);
 
 		body.AddForce (move * speed);
+		if (boostTimeout > 0) {
+			boostTimeout -= Time.deltaTime;
+		}
 	}
 
-	void OnTriggerEnter (Collider other) {
-		if (other.tag.Equals ("Respawn")) {
-			this.transform.position = startPos;
-			body.velocity = Vector3.zero;
-			body.angularVelocity = Vector3.zero;
-		}
+	public void respawn () {
+		this.transform.position = startPos;
+		body.velocity = Vector3.zero;
+		body.angularVelocity = Vector3.zero;
 	}
 }
---
+++ Assets/Scripts/ScoreKeeping.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeping : MonoBehaviour {
+
+	private void OnTriggerEnter (Collider other) {
+		if (other.tag.Equals ("Player")) {
+			other.GetComponent<PlayerControl> ().respawn ();
+			//change the scores accordingly
+		} else {
+			Destroy (other.gameObject);
+		}
+	}
+}
